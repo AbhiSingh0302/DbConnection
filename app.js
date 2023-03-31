35952ea82421aabdbc27f9e3767b16445591008d
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-31 10:17:55 +0530

    buyers table created in mysql

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const errorController = require('./controllers/error');
 const app = express();
 
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -23,9 +25,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User,{
+    constraints: true,
+    onDelete: 'CASCADE'
+})
+User.hasMany(Product);
+
+sequelize.sync({force: true})
 .then(result => {
-    app.listen(3000);
+    app.listen(4000);
 })
 .catch(err => {
     console.log(err);
